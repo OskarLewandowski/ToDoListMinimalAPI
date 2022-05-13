1cb5a5d9d824ea19efecfd04ad30cba3e90864a5
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-13 12:24:50 +0200

    Add project files.

---
+++ ToDoListMinimalAPI/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoListMinimalAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IToDoService, ToDoService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("todos", (IToDoService service) => service.GetAll());
+app.MapGet("todos/{id}", ([FromServices] IToDoService service, [FromRoute] Guid id) => service.GetById(id));
+app.MapPost("todos", ([FromServices] IToDoService service, [FromBody] ToDo toDo) => service.Create(toDo));
+app.MapPut("todos/{id}", ([FromServices] IToDoService service, [FromRoute] Guid id, [FromBody] ToDo toDo) => service.Update(toDo));
+app.MapDelete("todos/{id}", ([FromServices] IToDoService service, [FromRoute] Guid id) => service.Delete(id));
+
+
+
+app.Run();
+
---
+++ ToDoListMinimalAPI/ToDo/ToDo.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoListMinimalAPI;
+
+public class ToDo
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Value { get; set; }
+    public bool IsCompleted { get; set; }
+    public string WhenAddedTime { get; set; } = DateTime.Now.ToLongDateString();
+}
